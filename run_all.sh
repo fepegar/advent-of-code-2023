e404b16dd3b7e1fe8ebf583a3c43c13c7ccc04ea
Fernando Pérez-García <fepegar@gmail.com>
2023-12-03 19:21:59 +0000

    Improve script

--- run_all.sh
+++ run_all.sh
@@ -2,11 +2,12 @@
 
 this_dir=$(realpath $(dirname $0))
 for day_dir in $(ls -d src/day_*/); do
-    echo "RUNNING $day_dir"
+    echo "===================="
+    echo
+    echo "Running ${day_dir}..."
+    echo
     cd $day_dir
     cargo run -q
     cd $this_dir
-    echo
-    echo "===================="
     echo
 done
