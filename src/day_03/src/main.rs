1549da92d73d9b3f085a5d73b8407d77b8abf543
Fernando Pérez-García <fepegar@gmail.com>
2023-12-03 16:29:10 +0000

    Add part 2 of day 3

--- src/day_03/src/main.rs
+++ src/day_03/src/main.rs
@@ -1,11 +1,17 @@
 use std::cmp;
 use std::string;
+use std::collections::HashMap;
 
 fn main() {
     println!("PART 1");
     println!("------");
     part_1("example.txt");
     part_1("input.txt");
+    println!("");
+    println!("PART 2");
+    println!("------");
+    part_2("example.txt");
+    part_2("input.txt");
 }
 
 
@@ -15,16 +21,9 @@ fn part_1(filename: &str) {
     let numbers = get_numbers_from_schematic(&schematic);
     let mut sum = 0;
     for number in numbers {
-        println!("{row}", row=number.row);
-        println!("{value}", value=number.value);
-        println!("Is part?");
         if is_part(&number, &schematic) {
             sum += number.value;
-            println!("Yes");
-        } else {
-            println!("No");
         }
-        println!("");
     }
     println!("{filename}: {sum}");
 }
@@ -115,24 +114,94 @@ fn is_part(number: &Number, schematic: &Vec<Vec<char>>) -> bool {
     for row in row_ini..=row_fin {
         let col_ini = cmp::max(number.col_ini - 1, 0);
         let col_fin = cmp::min(number.col_fin, num_cols - 1);
-        // println!("number col_fin: {col_fin}", col_fin=number.col_fin);
-        // println!("row_ini: {row_ini}", row_ini=row_ini);
-        // println!("row_fin: {row_fin}", row_fin=row_fin);
-        // println!("col_ini: {col_ini}", col_ini=col_ini);
-        // println!("col_fin: {col_fin}", col_fin=col_fin);
         for col in col_ini..=col_fin {
             let value = schematic[row as usize][col as usize];
-            // println!("value in row {row} and col {col}: {value}", row=row, col=col, value=value);
             match value {
                 '0'..='9' | '.' => (),
                 _ => {
-                    println!("number row: {row_ini}", row_ini=number.row);
-                    println!("number col_ini: {col_ini}", col_ini=number.col_ini);
-                    println!("value in row {row} and col {col}: {value}", row=row, col=col, value=value);
                     return true;
                 },
             }
         }
     }
     false
+}
+
+
+fn part_2(filename: &str) {
+    let contents = read_file(filename);
+    let schematic = get_schematic(contents);
+    let numbers = get_numbers_from_schematic(&schematic);
+    let numbers_and_gears = get_numbers_and_gears(&numbers, &schematic);
+    let gears_dict = get_gears_dict(&numbers_and_gears);
+    let mut sum = 0;
+    // Iterate over the gears_dict
+    for (_coords, values) in gears_dict.iter() {
+        let num_numbers = values.len();
+        match num_numbers {
+            0 => (),
+            1 => (),
+            2 => {
+                let first_value = values[0];
+                let second_value = values[1];
+                sum += first_value * second_value;
+            },
+            _ => (),
+        }
+    }
+    println!("{filename}: {sum}");
+}
+
+
+fn get_numbers_and_gears(numbers: &Vec<Number>, schematic: &Vec<Vec<char>>) -> Vec<NumberAndGear> {
+    let mut numbers_and_gears: Vec<NumberAndGear> = Vec::new();
+    for number in numbers {
+        let num_rows = schematic.len() as i32;
+        let num_cols = schematic[0].len() as i32;
+        let row_ini = cmp::max(number.row - 1, 0);
+        let row_fin = cmp::min(number.row + 1, num_rows - 1);
+        for row in row_ini..=row_fin {
+            let col_ini = cmp::max(number.col_ini - 1, 0);
+            let col_fin = cmp::min(number.col_fin, num_cols - 1);
+            for col in col_ini..=col_fin {
+                let value = schematic[row as usize][col as usize];
+                match value {
+                    '*' => {
+                        let number_and_gear = NumberAndGear {
+                            value: number.value,
+                            gear_row: row,
+                            gear_col: col,
+                        };
+                        numbers_and_gears.push(number_and_gear);
+                    },
+                    _ => (),
+                }
+            }
+        }
+    }
+    numbers_and_gears
+}
+
+
+struct NumberAndGear {
+    value: u32,
+    gear_row: i32,
+    gear_col: i32,
+}
+
+
+fn get_gears_dict(numbers_and_gears: &Vec<NumberAndGear>) -> HashMap<(i32, i32), Vec<u32>> {
+    let mut gears_dict = HashMap::new();
+    for number_and_gear in numbers_and_gears {
+        let key = (number_and_gear.gear_row, number_and_gear.gear_col);
+        if !gears_dict.contains_key(&key) {
+            let empty_list: Vec<u32> = Vec::new();
+            gears_dict.insert(key, empty_list);
+        }
+        let item = number_and_gear.value;
+        // Push item to vector in the value
+        let value = gears_dict.get_mut(&key).unwrap();
+        value.push(item);
+    }
+    gears_dict
 }
